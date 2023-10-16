3f00324c94eeb25556ae7a79290bc9e37f3a1d36
Nabil <nlamriben@vcinc.com>
2023-10-16 15:14:24 -0400

    noworking need fixin

---
+++ MRADS2/Controls/LengthConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MRADS2.Controls
+{
+    public class LengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double actualWidth = (double)value;
+            return actualWidth * 0.1; // factor
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class TickConfig
+    {
+        public double Width { get; set; }
+
+    }
+}
